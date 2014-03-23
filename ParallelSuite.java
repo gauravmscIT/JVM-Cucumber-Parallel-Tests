1d58e50ee144cc695075cc2ef4475bb99a1f1e71
Gaurav Kumar <gaurav.kumar@bbc.co.uk>
2014-03-23 19:29:18 +0000

    Create ParallelSuite.java

---
+++ ParallelSuite.java
@@ -0,0 +1,38 @@
+package bbc.iplayer.ibl.cucumber;
+
+import org.junit.runners.Suite;
+import org.junit.runners.model.InitializationError;
+import org.junit.runners.model.RunnerBuilder;
+import org.junit.runners.model.RunnerScheduler;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+public class ParallelSuite extends Suite {
+
+    public ParallelSuite(Class<?> class1, RunnerBuilder builder) throws InitializationError {
+
+        super(class1, builder);
+
+        setScheduler(new RunnerScheduler() {
+
+            private final ExecutorService service = Executors.newFixedThreadPool(5);
+
+            public void schedule(Runnable childStatement) {
+                service.submit(childStatement);
+            }
+
+            public void finished() {
+                try {
+                    while (!service.awaitTermination(600, TimeUnit.SECONDS)) {
+                        service.shutdownNow();
+                    }
+                } catch (InterruptedException ie) {
+                    service.shutdownNow();
+                    Thread.currentThread().interrupt();
+                }
+            }
+        });
+    }
+}
